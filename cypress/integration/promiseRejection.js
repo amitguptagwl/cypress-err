97f22d897b573386a3187f2dc1701271c2c03871
Amit Gupta <amitgupta.gwl@gmail.com>
2021-09-19 05:43:19 +0530

    initial setup

---
+++ cypress/integration/promiseRejection.js
@@ -0,0 +1,11 @@
+
+describe('example to-do app', () => {
+  it.only('displays two todo items by default', () => {
+    cy.visit('https://example.cypress.io/todo')
+    cy.get('.todo-list li').should('have.length', 2)
+    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+    cy.then( () => {
+      throw new Error("on my wish");
+    })
+  });
+});
